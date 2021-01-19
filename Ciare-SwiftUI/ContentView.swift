39337d713acad9c1c4ec3734a9b64cc41ee93eb1
Vinicius Silva Alencar <vinicius.s.alencar@outlook.com>
2021-01-19 10:29:48 -0400

    Initial Commit

---
+++ Ciare-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ciare-SwiftUI
+//
+//  Created by Vinicius Alencar on 19/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
